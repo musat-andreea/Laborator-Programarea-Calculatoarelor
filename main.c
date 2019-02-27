37d8094a3fc2098f46e2c358f585111a0f6a4799
musat-andreea <48068102+musat-andreea@users.noreply.github.com>
2019-02-27 22:06:31 +0200

    TIMP(minute/ore/secunde)

--- main.c
+++ main.c
@@ -1,24 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-    typedef struct nr_complex
-            {
-                int re;
-                int im;
-            } NR;
-
-    void main (void)
+typedef struct moment
     {
-        NR z1, z2, rezad, rezsc;
-        printf("Introduceti primul numar:\n");
-        scanf("%d %d",&z1.re,&z1.im);
-        printf("introduceti al doilea numar:\n");
-        scanf("%d %d",&z2.re,&z2.im);
-        rezad.re=z1.re+z2.re;
-        rezad.im=z1.im+z2.im;
-        rezsc.re=z1.re-z2.re;
-        rezsc.im=z1.im-z2.im;
-        printf("suma este %d+%d *i\n",rezad.re,rezad.im);
-        printf("diferenta este %d %d *i",rezsc.re,rezsc.im);
+        int ora;
+        int minut;
+        int secunda;
+    }MOMENT;
 
+
+void main(void)
+{
+    MOMENT z1, z2,sum;
+    printf("Introduceti primul moment al zilei\n");
+    printf("ora1:\n");
+    scanf("%d",&z1.ora);
+    printf("minut1:\n");
+    scanf("%d",&z1.minut);
+    printf("secunda1:\n");
+    scanf("%d",&z1.secunda);
+
+    printf("Introduceti al doilea  moment al zilei\n");
+    printf("ora2:\n");
+    scanf("%d",&z2.ora);
+    printf("minut2:\n");
+    scanf("%d",&z2.minut);
+    printf("secunda2:\n");
+    scanf("%d",&z2.secunda);
+
+    sum.secunda=z1.secunda+z2.secunda;
+    sum.minut=z1.minut+z2.minut;
+    sum.ora=z1.ora+z2.ora;
+    if(sum.secunda>60)
+    {
+        sum.minut++;
+        sum.secunda=sum.secunda-60;
     }
+    if(sum.minut>60)
+    {
+        sum.ora++;
+        sum.minut=sum.minut-60;
+    }
+
+    printf("suma celor doua momente de timp este:");
+    printf("%d %d %d",sum.ora,sum.minut,sum.secunda);
+}
